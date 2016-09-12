516a1297b80ba73cbcd273d3b0760ca11ef4946f
Alexander Sirenko <alexander.sirenko@gmail.com>
2016-09-12 23:40:05 +0700

    Add restauraunts activity.

--- app/src/main/java/com/example/sirenko/tourguide/MainActivity.java
+++ app/src/main/java/com/example/sirenko/tourguide/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.sirenko.tourguide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     public void openCategory(View view) {
         switch(view.getId()) {
             case R.id.tv_cat_restaurants:
+                Intent openRest = new Intent(MainActivity.this, RestaurantsActivity.class);
+                startActivity(openRest);
             case R.id.tv_cat_museums:
             case R.id.tv_cat_cinemas:
             case R.id.tv_cat_hospitals:
