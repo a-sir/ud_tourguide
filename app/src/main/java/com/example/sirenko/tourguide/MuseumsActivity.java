c0c12276fa8990028b4233903c542a4b30f78490
Alexander Sirenko <alexander.sirenko@gmail.com>
2016-09-18 11:28:18 +0700

    List of museums.

--- app/src/main/java/com/example/sirenko/tourguide/MuseumsActivity.java
+++ app/src/main/java/com/example/sirenko/tourguide/MuseumsActivity.java
@@ -2,12 +2,25 @@ package com.example.sirenko.tourguide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MuseumsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_museums);
+        setContentView(R.layout.place_list);
+        List<Place> places = new ArrayList<>();
+        places.add(Place.museum("Ethnographic museum", "Old pictures and houses", "Every day 9-22", "Hanoi, Giang Vo, 161", "www.singapore-food-hanoi.com.vn", "+848762378675"));
+        places.add(Place.museum("Army museum", "Tanks and airplanes", "Every day 9-22", "Hanoi, Giang Vo, 161", "www.singapore-food-hanoi.com.vn", "+848762378675"));
+        places.add(Place.museum("Internet museum", "About pokemons and coccoc", "Every day 9-22", "Hanoi, Giang Vo, 161", "www.singapore-food-hanoi.com.vn", "+848762378675"));
+        places.add(Place.museum("Boring museum", "History of history", "Every day 9-22", "Hanoi, Giang Vo, 161", "www.singapore-food-hanoi.com.vn", "+848762378675"));
+        PlaceAdapter adapter = new PlaceAdapter(this, places);
+        ListView layout = (ListView) findViewById(R.id.places_list_id);
+        assert layout != null;
+        layout.setAdapter(adapter);
     }
 }
