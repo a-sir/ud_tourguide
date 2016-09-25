acd7611e31b58cc0bd9b80e8542e0b60d3c756a5
Alexander Sirenko <alexander.sirenko@gmail.com>
2016-09-25 17:32:19 +0700

    Add info about hospitals.

--- app/src/main/java/com/example/sirenko/tourguide/HospitalActivity.java
+++ app/src/main/java/com/example/sirenko/tourguide/HospitalActivity.java
@@ -4,6 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,10 +20,24 @@ public class HospitalActivity extends AppCompatActivity {
         setContentView(R.layout.place_list);
         List<Place> places = new ArrayList<>();
 
-        places.add(Place.hospital("Platinum cineplex Royal City", "The best cinema in Hanoi", "Every day 9-22", "Hanoi, Giang Vo, 161", "www.singapore-food-hanoi.com.vn", "+848762378675", "+848762378675", R.drawable.hospital_french));
-        places.add(Place.hospital("CGV in Mipec Tower", "New Hollywood movies", "Every day 9-22", "Hanoi, Giang Vo, 161", "www.singapore-food-hanoi.com.vn", "+848762378675", "+848762378675", R.drawable.hospital_french));
-        places.add(Place.hospital("Old cinema on Lang Ha", "Low prices", "Every day 9-22", "Hanoi, Giang Vo, 161", "www.singapore-food-hanoi.com.vn", "+848762378675", "+848762378675", R.drawable.hospital_french));
-        places.add(Place.hospital("CGV Vincom center", "In city center", "Every day 9-22", "Hanoi, Giang Vo, 161", "www.singapore-food-hanoi.com.vn", "+848762378675", "+848762378675", R.drawable.hospital_french));
+        try {
+            JSONObject root = new JSONObject(ResourceUtils.read(getResources().openRawResource(R.raw.data)));
+            JSONArray museums = root.getJSONArray("hospitals");
+            for (int i = 0 ; i < museums.length(); ++i) {
+                JSONObject m = museums.getJSONObject(i);
+                places.add(Place.hospital(
+                        m.getString("name"),
+                        m.getString("description"),
+                        m.getString("workingHours"),
+                        m.getString("address"),
+                        m.getString("website"),
+                        m.getString("phone"),
+                        m.getString("emergencyPhone"),
+                        -1));
+            }
+        } catch (JSONException | IOException e) {
+            throw new RuntimeException(e);
+        }
         PlaceAdapter adapter = new PlaceAdapter(this, places);
         ListView layout = (ListView) findViewById(R.id.places_list_id);
         assert layout != null;
